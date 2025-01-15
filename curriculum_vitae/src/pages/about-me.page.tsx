9c33f2d7bab82fd22b0e8db59bf23328caaff73c
51n0nGG0 <eloyschmidtrodriguez@gmail.com>
2025-01-15 19:57:08 +0100

    feat:Create about me base info

--- curriculum_vitae/src/pages/about-me.page.tsx
+++ curriculum_vitae/src/pages/about-me.page.tsx
@@ -3,11 +3,28 @@ import ProfileCard from "../components/profile-card.component";
 export default function AboutMe () {
     return (
         <>
-            <div className="grid grid-cols-2 h-full relative">
+            <div className="grid grid-cols-[3fr_4fr] h-full relative">
                 <div className="bg-[#e6cece]"></div>
                 <div></div>
-                <div className="absolute w-full flex justify-center">
+                <div className="absolute w-full flex justify-center gap-x-10">
                     <ProfileCard/>
+                    <div className="mb-[52px] mt-[72px]">
+                        <p className="text-[100px] font-bold font-poppins">Hello</p>
+                        <p className="text-[25px] font-poppins mb-[38px]">This is who I am and what I do</p>
+                        <div className="flex gap-x-5 mb-[56px]">
+                            <a className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-red-700 w-[130px] text-white h-[35px] rounded-full">
+                                <span>Curriculum</span>
+                            </a>
+                            <a className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-red-700 w-[130px] text-white h-[35px] rounded-full">
+                                <span>Projects</span>
+                            </a>
+                        </div>
+                        <div className="flex flex-col gap-y-8 w-[369px]">
+                            <p className="text-[17px] font-montserrat">Párrafo. Haz clic aquí para agregar tu propio texto y editar. Es fácil. Haz clic en "Editar texto" o doble clic aquí para agregar tu contenido y cambiar la fuente.</p>
+                            <p className="text-[17px] font-montserrat">Aquí puedes contar tu historia y permitir que tus usuarios sepan más sobre ti.</p>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </>
