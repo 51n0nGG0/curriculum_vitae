8faeda531427dabde541d0af4ccde2793e6a1b92
Eloy Alfredo Schmidt Rodríguez <eloyschmidtrodriguez@gmail.com>
2025-01-22 22:16:54 +0100

    fix:Problem with links in mobile

--- curriculum_vitae/src/components/profile-card.component.tsx
+++ curriculum_vitae/src/components/profile-card.component.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { Link } from "react-router-dom";
 
 export default function ProfileCard() {
     return (
@@ -12,12 +13,12 @@ export default function ProfileCard() {
                 <h1 className="text-2xl text-center font-bold font-poppins m-0 max-sm:text-xl"><span>Eloy Alfredo <br></br> Schmidt Rodríguez</span></h1>
                 <div className="w-[50px] h-[5px] bg-red-700 mb-[25px]"></div>
                 <div className="flex gap-x-5 mb-[38px] lg:hidden">
-                    <a className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-red-700 w-[130px] text-white h-[35px] border-solid border-2 border-red-700 rounded-full hover:bg-white hover:text-black ease-in duration-150 max-sm:text-[12px] max-sm:w-[120px]">
+                    <Link to="/curriculum" className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-red-700 w-[130px] text-white h-[35px] border-solid border-2 border-red-700 rounded-full hover:bg-white hover:text-black ease-in duration-150 max-sm:text-[12px] max-sm:w-[120px]">
                         <span>Curriculum</span>
-                    </a>
-                    <a className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-white w-[130px] text-black h-[35px] border-solid border-2 border-black rounded-full hover:bg-red-700 hover:text-white hover:border-red-700 ease-in duration-150 max-sm:text-[12px] max-sm:w-[120px]">
+                    </Link>
+                    <Link to="/projects" className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-white w-[130px] text-black h-[35px] border-solid border-2 border-black rounded-full hover:bg-red-700 hover:text-white hover:border-red-700 ease-in duration-150 max-sm:text-[12px] max-sm:w-[120px]">
                         <span>Projects</span>
-                    </a>
+                    </Link>
                 </div>
                 <p className="uppercase text-xl font-montserrat m-0 mb-[51px] max-lg:hidden">Software Engineer</p>
                 <div className="w-full bg-[#faf6f6]">
