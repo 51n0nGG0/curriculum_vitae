c98a6320f30173a6e75e9965bbff03ee02eae44b
Eloy Alfredo Schmidt Rodríguez <eloyschmidtrodriguez@gmail.com>
2025-02-03 14:02:13 +0100

    feat: Add translations

--- curriculum_vitae/src/components/profile-card.component.tsx
+++ curriculum_vitae/src/components/profile-card.component.tsx
@@ -3,8 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function ProfileCard() {
+
+    const { t } = useTranslation();
+
     return (
         <>
             <div className="grid grid-cols-full justify-items-center h-[auto] w-[375px] bg-[#f4e6e6] shadow-lg position-absolute max-sm:w-[320px]">
@@ -13,13 +17,13 @@ export default function ProfileCard() {
                 <div className="w-[50px] h-[5px] bg-red-700 mb-[25px]"></div>
                 <div className="flex gap-x-5 mb-[38px] lg:hidden">
                     <Link to="/curriculum" className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-red-700 w-[130px] text-white h-[35px] border-solid border-2 border-red-700 rounded-full hover:bg-white hover:text-black ease-in duration-150 max-sm:text-[12px] max-sm:w-[120px]">
-                        <span>Curriculum</span>
+                        <span>{t("header.curriculum")}</span>
                     </Link>
                     <Link to="/projects" className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-white w-[130px] text-black h-[35px] border-solid border-2 border-black rounded-full hover:bg-red-700 hover:text-white hover:border-red-700 ease-in duration-150 max-sm:text-[12px] max-sm:w-[120px]">
-                        <span>Projects</span>
+                        <span>{t("header.projects")}</span>
                     </Link>
                 </div>
-                <p className="uppercase text-xl font-montserrat m-0 mb-[51px] max-lg:hidden">Software Engineer</p>
+                <p className="uppercase text-xl font-montserrat m-0 mb-[51px] max-lg:hidden">{t("header.profession")}</p>
                 <div className="w-full bg-[#faf6f6]">
                     <div className="mt-[14px] mb-[14px] flex justify-center gap-x-5">
                         <Link to="https://www.linkedin.com/in/eloyalfredoschmidtr/" target="_blank" rel="noopener noreferrer">
--- curriculum_vitae/src/pages/about-me.page.tsx
+++ curriculum_vitae/src/pages/about-me.page.tsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import ProfileCard from "../components/profile-card.component";
+import { useTranslation } from "react-i18next";
+
 
 export default function AboutMe () {
+
+    const { t } = useTranslation();
+
     return (
         <>
             <div className="absolute grid grid-cols-[3fr_4fr] h-full w-full z-0 max-lg:grid-rows-[1fr_5fr] max-lg:grid-cols-[1fr]">
@@ -10,20 +15,19 @@ export default function AboutMe () {
             <div className="flex flex-row self-center w-full flex justify-center items-start gap-x-10 z-10 relative max-lg:flex-col max-lg:items-center">
                     <ProfileCard/>
                     <div className="">
-                        <p className="text-[100px] font-bold font-poppins max-sm:text-[90px]">Hello</p>
-                        <p className="text-[25px] font-poppins mb-[38px] max-sm:text-[20px]">This is who I am and what I do</p>
+                        <p className="text-[100px] font-bold font-poppins max-sm:text-[90px]">{t("aboutme.title")}</p>
+                        <p className="text-[25px] font-poppins mb-[38px] max-sm:text-[20px]">{t("aboutme.subtitle")}</p>
                         <div className="flex gap-x-5 mb-[38px] max-lg:hidden" >
                             <Link to="/curriculum" className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-red-700 w-[130px] text-white h-[35px] border-solid border-2 border-red-700 rounded-full hover:bg-white hover:text-black ease-in duration-150">
-                                <span>Curriculum</span>
+                                <span>{t("header.curriculum")}</span>
                             </Link>                            
                             <Link to="/projects" className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-white w-[130px] text-black h-[35px] border-solid border-2 border-black rounded-full hover:bg-red-700 hover:text-white hover:border-red-700 ease-in duration-150">
-                                <span>Projects</span>
+                                <span>{t("header.projects")}</span>
                             </Link>
                         </div>
                         <div className="flex flex-col gap-y-5 w-[369px] mb-[38px] max-sm:w-[330px]">
-                            <p className="text-[16px] font-montserrat max-sm:text-[14px]">I'm Eloy Alfredo Schmidt Rodríguez, a 24-year-old Software Engineer with a passion for continuous learning and problem-solving. I enjoy listening to music, watching anime, and reading books. I'm always eager to expand my knowledge by working on personal projects and taking courses.
-                            </p> 
-                            <p className="text-[16px] font-montserrat max-sm:text-[14px]">Currently, I'm completing a Master's in Web Engineering at the University of Oviedo, further strengthening my skills and expertise in the field.</p>
+                            <p className="text-[16px] font-montserrat max-sm:text-[14px]">{t("aboutme.paragraph1")}</p> 
+                            <p className="text-[16px] font-montserrat max-sm:text-[14px]">{t("aboutme.paragraph2")}</p>
                         </div>
                     </div>
                 </div>
