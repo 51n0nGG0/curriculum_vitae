c63d0e41a75d9eb94533eb7c0cbd2fbd551cbe32
Eloy Alfredo Schmidt Rodríguez <eloyschmidtrodriguez@gmail.com>
2025-02-05 21:24:57 +0100

    feat: Make responsive curriculum page

--- curriculum_vitae/src/components/info-panel.component.tsx
+++ curriculum_vitae/src/components/info-panel.component.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons/faAngleDown";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons/faAngleUp";
 
 interface InfoPanelProps {
     years: string;
@@ -6,25 +9,55 @@ interface InfoPanelProps {
     subtitle: string;
     location: string;
     info: string;
+    image?: string;
 }
 
-const InfoPanel: React.FC<InfoPanelProps> = ({years, title, subtitle, location, info}) => {
+const InfoPanel: React.FC<InfoPanelProps> = ({years, title, subtitle, location, info, image}) => {
+
+    const [ moreInfoOpen, setMoreInfoOpen] = useState(false);
 
     return (
         <>
-            <div className="w-[724px] sm:max-md:w-[470px] max-sm:w-[350px] h-auto flex flex-row max-md:flex-col bg-white p-7">
-                <div className="flex-1 flex flex-col gap-y-2">
-                    <h3 className="text-2xl font-bold text-red-700">{years}</h3>
-                    <p className="uppercase">{title}</p>
-                    <p>{subtitle}</p>
-                    <p>{location}</p>
+            <div className={"w-[724px] sm:max-md:w-[470px] max-sm:w-[350px] flex flex-col bg-white p-7 gap-y-7 shadow-lg"}>
+                <div className="flex flex-row gap-x-7 max-sm:flex-col max-sm:gap-y-7">
+                    {
+                        image ? 
+                            <>
+                                <img className="w-[160px] h-[160px] rounded-md max-sm:w-full max-sm:h-[180px] object-cover" src={image} alt="" />
+                            </>
+                            : 
+                            <></>
+                    }
+                    <div className="flex-1 flex flex-col gap-y-2">
+                        <h3 className="text-2xl font-bold text-red-700">{years}</h3>
+                        <p className="uppercase font-semibold">{title}</p>
+                        <p>{subtitle}</p>
+                        <p>{location}</p>
+                    </div>
                 </div>
-                <div className="flex-1 flex flex-col gap-y-2 mt-10">
-                    {info!.split('\n').map((paragraph, index)=> (
-                        <>
-                            <p key={index}>{paragraph}</p>
-                        </>
-                    ))}
+                <div className={`flex flex-col gap-y-2 relative ${!info ? "hidden": ""}`}>
+                    {
+                        moreInfoOpen ? 
+                            info!.split('\n').map((paragraph, index)=> (
+                                <>
+                                    <p key={index}>{paragraph}</p>
+                                </>
+                            ))
+                            :
+                            <>
+                                <div className="size-full absolute bg-gradient-to-b from-transparent to-white"></div>
+                                <p className="h-[100px] overflow-hidden ">{info!.split('\n')[0]}</p>
+                            </>
+                    }        
+                </div>
+                <div className={`flex-1 flex justify-center items-center gap-x-2 ${!info ? "hidden": ""}`}>
+                    
+                        <button className="text-[14px] flex justify-center items-center uppercase font-semibold font-montserrat bg-red-700 w-[150px] text-white h-[35px] border-solid border-2 border-red-700 rounded-full hover:bg-white hover:text-black ease-in duration-150 gap-x-2" type="button" onClick={()=> setMoreInfoOpen(!moreInfoOpen)}>
+                            <FontAwesomeIcon className="stroke-current" icon={!moreInfoOpen ? faAngleDown : faAngleUp} size="lg"/>
+                            { moreInfoOpen ? <span>Less info</span> : <span>More info</span> }
+                        </button>
+                    
+                    
                 </div>
             </div>
         </>
--- curriculum_vitae/src/pages/curriculum.page.tsx
+++ curriculum_vitae/src/pages/curriculum.page.tsx
@@ -1,5 +1,8 @@
 import { useTranslation } from "react-i18next";
 import InfoPanel from "../components/info-panel.component";
+import IngeniumLogo from "../assets/images/ingenium-logo.jpeg";
+import BabelLogo from "../assets/images/babel-logo.jpg";
+import UniversityLogo from "../assets/images/university-logo.jpg"
 
 export default function Curriculum () {
 
@@ -15,13 +18,13 @@ export default function Curriculum () {
                 </div>
                 <div className="flex flex-col gap-y-12 max-md:gap-y-7">
                     <h2 className="text-2xl font-bold">{t("curriculum.experience")}</h2>
-                    <InfoPanel years="Feb 2023 - Dic 2023" title={t("curriculum.fullstack-dev")} subtitle="Babel Sistemas de Información" location="Oviedo" info={t("curriculum.job-description-1")}/>
-                    <InfoPanel years="Feb 2022 - Jun 2022" title="Web Developer" subtitle="Ingenium S.L." location="Lugo de LLanera" info=""/>
+                    <InfoPanel years="Feb 2023 - Dic 2023" title={t("curriculum.fullstack-dev")} subtitle="Babel Sistemas de Información" location="Oviedo" info={t("curriculum.job-description-1")} image={BabelLogo}/>
+                    <InfoPanel years="Feb 2022 - Jun 2022" title="Web Developer" subtitle="Ingenium S.L." location="Lugo de LLanera" info="" image={IngeniumLogo}/>
                 </div>
                 <div className="flex flex-col gap-y-12 max-md:gap-y-7">
                     <h2 className="text-2xl font-bold">{t("curriculum.education")}</h2>
-                    <InfoPanel years="2023-2025" title="Universidad de Oviedo" subtitle="Máster Universitario en Ingeniería Web" location="Oviedo" info=""/>
-                    <InfoPanel years="2018-2024" title="Universidad de Oviedo" subtitle="Grado en Ingeniería Informática del Software" location="Oviedo" info=""/>
+                    <InfoPanel years="2023-2025" title="Universidad de Oviedo" subtitle="Máster Universitario en Ingeniería Web" location="Oviedo" info=""  image={UniversityLogo}/>
+                    <InfoPanel years="2018-2024" title="Universidad de Oviedo" subtitle="Grado en Ingeniería Informática del Software" location="Oviedo" info="" image={UniversityLogo}/>
                 </div>
             </div>
         </>
